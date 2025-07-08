0fc5cc089621010dfd3f9b9a1a34d9a283eab7c8
MaximosVeLOL <Maximusvegapoopyhead@gmail.com>
2025-07-07 19:28:02 -0500

    Remove builds

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_IntToolGUI_FILE C:/Users/gerav/OneDrive/Documents/IntToolGUI/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/IntToolGUI.exe)
-set(__QT_DEPLOY_TARGET_IntToolGUI_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_IntToolGUI_RUNTIME_DLLS D:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
